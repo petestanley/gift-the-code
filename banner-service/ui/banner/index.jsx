930369577eca5b5b2ce65cc2c584b763e8871419
xzs424 <xizhongshiwise@gmail.com>
2018-11-18 11:40:56 -0500

    fix style

--- banner-service/ui/banner/index.jsx
+++ banner-service/ui/banner/index.jsx
@@ -2,9 +2,15 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-
+import { withStyles } from '@material-ui/core/styles';
 
 const getUrl = 'http://localhost:4000/banners?category=prostate_cancer,testicular_cancer,mental_health'
+
+const styles = theme => ({
+  text: {
+    fontSize: "5em",
+  }
+});
 
 class Banner extends Component {
 
@@ -34,23 +40,27 @@ class Banner extends Component {
 
   render() {
 
+    const { classes } = this.props
+
     return (
 
+
+
           <Paper>
-            <Typography variant="h5" component="h3">
+            <Typography className={classes.text} variant="h2" component="h2">
               {this.state.banner.category} has news!
             </Typography>
-            <Typography component="p">
+            <Typography className={classes.text} variant="h3" component="h3">
               {this.state.banner.content}
             </Typography>
           </Paper>
-
       )
   }
-
 }
 
+const BannerPage = withStyles(styles)(Banner)
+
 ReactDOM.render(
-  <Banner />,
+  <BannerPage />,
   document.getElementById('banner')
 );
